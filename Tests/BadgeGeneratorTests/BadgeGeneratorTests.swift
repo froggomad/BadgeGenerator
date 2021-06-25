b6ea6a2f492a469e0c3595823a5dc9fad11de7dc
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-06-25 15:45:26 -0700

    added helper method to create out

--- Tests/BadgeGeneratorTests/BadgeGeneratorTests.swift
+++ Tests/BadgeGeneratorTests/BadgeGeneratorTests.swift
@@ -1,13 +1,25 @@
     import XCTest
-    @testable import BadgeGenerator
-
+    import BadgeGenerator
+    
     final class BadgeGeneratorTests: XCTestCase {
+        
         func testBadgeValueDoesChange() {
-            let view = UIView()
-            let badge = view.setBadge(in: .southWest, with: "1")
+            
+            let badge = sut()
             
             XCTAssertEqual(badge.text, "1")
             badge.text = "2"
             XCTAssertEqual(badge.text, "2")
+            
         }
+        
+        private func sut(direction: BadgeDirection = .northWest, text: String = "1", file: StaticString = #file, line: UInt = #line) -> BadgeLabel {
+            
+            let view = UIView()
+            let badge = view.setBadge(in: direction, with: text)
+            
+            return badge
+            
+        }
+        
     }
