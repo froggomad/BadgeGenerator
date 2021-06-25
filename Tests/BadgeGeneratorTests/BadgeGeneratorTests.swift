70870303b90697d6ca8ffe656304e0bdbce7623d
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-06-25 15:46:11 -0700

    added extension to test for memory leaks

--- Tests/BadgeGeneratorTests/BadgeGeneratorTests.swift
+++ Tests/BadgeGeneratorTests/BadgeGeneratorTests.swift
@@ -4,22 +4,36 @@
     final class BadgeGeneratorTests: XCTestCase {
         
         func testBadgeValueDoesChange() {
-            
             let badge = sut()
             
             XCTAssertEqual(badge.text, "1")
             badge.text = "2"
             XCTAssertEqual(badge.text, "2")
+        }
+        
+        func testBadgeDoesNotLeak() {
+            let badge = sut()
             
+            badge.remove()
+            assertNoMemoryLeak(badge)
         }
         
         private func sut(direction: BadgeDirection = .northWest, text: String = "1", file: StaticString = #file, line: UInt = #line) -> BadgeLabel {
-            
             let view = UIView()
             let badge = view.setBadge(in: direction, with: text)
             
-            return badge
+            assertNoMemoryLeak(view, file: file, line: line)
+            assertNoMemoryLeak(badge, file: file, line: line)
             
+            return badge
         }
         
+    }
+    
+    extension XCTestCase {
+        func assertNoMemoryLeak(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+            addTeardownBlock { [weak instance] in
+                XCTAssertNil(instance, "Instance should have been deallocated. Potential retain cycle.", file: file, line: line)
+            }
+        }
     }
